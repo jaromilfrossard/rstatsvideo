dd55b4af23cff4123ce83e2c997e4010cd1cb550
jaromilfrossard <jaromil.frossard@gmail.com>
2022-01-31 15:42:42 +0100

    update function

--- function/tibble_channel_stats.R
+++ function/tibble_channel_stats.R
@@ -1,7 +1,13 @@
 tibble_channel_stats <- function(id_channel){
-  stats <- tuber::get_channel_stats(id_channel)
+  quiet <- function(x) { 
+    sink(tempfile()) 
+    on.exit(sink()) 
+    invisible(force(x)) 
+  } 
+  
+  
+  stats <- quiet(tuber::get_channel_stats(id_channel))
   tibble(count_video = as.integer(stats$statistics$videoCount),
          count_substriber = as.integer(stats$statistics$subscriberCount))
-         
          
 }
