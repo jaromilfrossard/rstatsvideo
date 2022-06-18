aad7d07baec6d67f4131009367d89afd67d6cd8a
jaromilfrossard <jaromil.frossard@gmail.com>
2022-06-18 18:07:51 +0200

    Update 2022_06_18

--- workflow_new.R
+++ workflow_new.R
@@ -72,7 +72,7 @@ validate_new_videos()#6aJMGdCxbgA
 # tuber::get_video_details("6aJMGdCxbgA")
 
 videos <- choose_videos(max_tweet = 3)%>%
-  filter(!id_video%in%c("6Tg8xiyWLtM","6Tg8xiyWLtM"))
+  filter(!id_video%in%c("-FuEXMVbh4o"))
 #videos <- videos%>%filter(new_video)
 
 # 
