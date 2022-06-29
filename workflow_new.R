24c3ea343918122142a900a83a33ae053bb2fe53
jaromilfrossard <jaromil.frossard@gmail.com>
2022-06-29 14:15:49 +0200

    Update 2022_06_29

--- workflow_new.R
+++ workflow_new.R
@@ -63,7 +63,7 @@ validate_new_channels()
 #update channels videos
 
 
-walk(tb_channel$id_channel[-c(1:71)], update_channel_video)
+walk(tb_channel$id_channel, update_channel_video)
 #which(tb_channel$id_channel=="UClGaKSIiaYp8o2SB3D3aS_A")
 
 
