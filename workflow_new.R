4b7ffacd6ea3faa4613c21c4c1899f444f4a4e5f
jaromilfrossard <jaromil.frossard@gmail.com>
2022-12-22 20:14:57 +0100

    Update 2022_12_22

--- workflow_new.R
+++ workflow_new.R
@@ -68,7 +68,7 @@ walk(tb_channel$id_channel, update_channel_video)
 validate_new_videos()#6aJMGdCxbgA
 
 videos <- choose_videos(max_tweet = 5)%>%
-  filter(!id_video%in%c("8iF-483Pf8w","nqJsLSLd39A")) # 28 july
+  filter(!id_video%in%c("nqJsLSLd39A","xrxoSupeZYo","7tQj0CqB4Vc")) # 28 july
 #videos <- videos%>%filter(new_video)
 
 # create txt
